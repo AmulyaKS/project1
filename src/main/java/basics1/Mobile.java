9e83ef8c31bd29b19fc8c03dcefe57aea96fcfe7
AmulyaKS <amulyaks2001@gmail.com>
2023-05-27 10:41:19 +0530

    push code

---
+++ src/main/java/basics1/Mobile.java
@@ -0,0 +1,13 @@
+package basics1;
+
+public class Mobile 
+{
+	public Mobile()
+	{
+		System.out.println("Object is created..");
+	}
+	void musicPlayer()
+	{
+		System.out.println("abcd....");
+	}
+}
